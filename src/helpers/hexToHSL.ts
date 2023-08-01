3fe1c2d85e293e6c4860798ca3863ea294efd26a
Connor <connordanielfrank@gmail.com>
2023-08-02 01:08:01 +1200

    Adjusts hexToHSL

--- src/helpers/hexToHSL.ts
+++ src/helpers/hexToHSL.ts
@@ -1,4 +1,4 @@
-export default function hexToHSL(hex, lightnessPercent = 10) {
+export default function hexToHSL(hex, lightnessPercent = 12.5) {
   let r = parseInt(hex.slice(1, 3), 16);
   let g = parseInt(hex.slice(3, 5), 16);
   let b = parseInt(hex.slice(5, 7), 16);

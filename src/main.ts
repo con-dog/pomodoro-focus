0a161fd9eb4d6a071b39cd9aab1eb0eb7fe1d73e
Connor <connordanielfrank@gmail.com>
2023-08-06 17:22:29 +1200

    Ready for publishing

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
+/*!
+Connor Talbot | Pomodoro Focus | property of hackhive.io hello@hackhive.io | Copyright Disclaimer
+
+This Copyright Disclaimer (“Disclaimer”) sets forth the rights, limitations, and obligations concerning the use of copyrighted materials owned by Connor Talbot / hackhive.io (“Owner”). By accessing or using any materials, content, or intellectual property owned by the Owner, you acknowledge and agree to the terms of this Disclaimer.
+
+Copyright Ownership: All materials, content, and intellectual property, including but not limited to text, images, graphics, logos, audio, video, and software, made available in the Pomodoro Focus extension, hackhive.io website, publications, or other platforms, are protected by copyright laws and owned by the Owner unless otherwise stated.
+
+Permission for Personal Use: You may not download, reverse-engineer, copy, modify, or print copyrighted materials from Connor Talbots/hackhive.io/Pomodoro Focus platforms or extensions for any personal, non-commercial, or commercial use.
+
+Disclaimer of Liability: The Owner shall not be liable for any direct, indirect, incidental, consequential, or special damages arising out of or in connection with the use of copyrighted materials, including but not limited to errors, omissions, or inaccuracies in the content or loss or damage of any kind incurred as a result of the use of any copyrighted material.
+
+By accessing or using any copyrighted materials owned by Pomodoro Focus extension or Connor Talbot or hackhive.io, you acknowledge and agree to abide by the terms of this Copyright Disclaimer. If you do not agree with any of these terms, please refrain from accessing or using the copyrighted materials.
+
+For any inquiries regarding this Copyright Disclaimer, please contact 'hello' at hello@hackhive.io
+
+Date: 08.06.2023 deviceview | Connor Talbot | Auckland, New Zealand
+*/
+
 import './app.css'
 import App from './App.svelte'
 
